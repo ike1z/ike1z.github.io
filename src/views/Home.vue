59934db239fa1756e3ad8e7d0927006dd6351b22
K.Ikeda <kazuki.ikeda@bebit.com>
2023-05-22 12:42:14 +0900

    add initial settings

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+console.log("home");
+</script>
+
+<template>
+  <div class="home">aaa</div>
+</template>
+
+<style>
+.home {
+  color: #aaa;
+  margin-top: 20px;
+}
+</style>
